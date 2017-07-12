944c43f65631ff449c888ec54ba7297a1b91be2d
Victor Ermolaev <victor.ermolaev@nedap.com>
2017-07-12 11:56:14 +0200

    Update the algorithm slightly

--- src/minimalPerimeterTriangleTest.ts
+++ src/minimalPerimeterTriangleTest.ts
@@ -22,18 +22,17 @@ test("verifies that a line is not tangent to a convex hull", t => {
 });
 
 test("compute minimal perimeter triangle condinitoned on the base", t => {
+    // this test contains two funny examples of counter intuitive but correct results
+    // 1. the minimal perimeter triangle is generated for the side new Vec2(2, 1), new Vec2(2, 0)
+    // 2. the min triangle is different from the expected one by slight shift of the expected tip new Vec2(0, 2.5)
+    //    to the left (due counter clockwise search). But such shift indeed delivers smaller perimeter!
+
     const points = [
-        new Vec2(-2, 0), new Vec2(-2, 1), new Vec2(-1, 2), new Vec2(0, 2.5),
-        new Vec2(1, 2), new Vec2(2, 1), new Vec2(2, 0)];
+        new Vec2(2, 1), new Vec2(2, 0), new Vec2(-2, 0), new Vec2(-2, 1),
+        new Vec2(-1, 2), new Vec2(0, 2.5), new Vec2(1, 2)];
 
-    const {A, B, C} = minTriangleWithBase(points, 10 ** -5, 0.1)!;
-
-    t.true(Math.abs(0 - A.x) < 0.2);
-    t.true(Math.abs(3 - A.y) < 0.2);
-
-    t.true(Math.abs(-3 - B.x) < 0.1);
-    t.true(Math.abs(0 - B.y) < 0.1);
-
-    t.true(Math.abs(3 - C.x) < 0.11);
-    t.true(Math.abs(0 - C.y) < 0.1);
+    const { A, B, C } = minTriangleWithBase(points, 10 ** -5, 0.1)!;
+    t.true(A.minus(new Vec2(-2.893530107256646, 0)).norm < 0.1);
+    t.true(B.minus(new Vec2(3, 0)).norm < 0.1);
+    t.true(C.minus(new Vec2(-0.11245650896737835, 3.1124565089673784)).norm < 0.1);
 });
